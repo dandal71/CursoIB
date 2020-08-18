61d80966590a9bf102338c8974914fd7a198ca54
diego <diego@familia-desktop>
2020-08-18 16:59:02 -0300

    Inicializacion

---
+++ hola-mundo/src/main/java/com/ib/App.java
@@ -0,0 +1,41 @@
+package com.ib;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    
+    public static final ApplicationContext CONTEXT;
+    
+    static {
+        CONTEXT = new ClassPathXmlApplicationContext("app-config.xml");
+    }
+    
+    @Autowired private Sumadora sumadora;
+    
+    public static void main( String[] args ) {
+        // Con Spring ya no hace fala
+        /*
+        App app = new App();
+        app.run();
+        */
+        App app = CONTEXT.getBean(App.class);
+        app.run();
+    }
+
+    // Con Spring ya no hace fala
+    /*
+    public App() { 
+        //this.sumadora = new Sumadora();
+    }
+    */
+    
+    public void run() {
+        int a = 1;
+        int b = 3;
+        //Sumadora sumadora = new Sumadora();
+        int r = sumadora.sumar(a, b);
+        System.out.println("Resultado: " + r);
+    }
+}
---
+++ hola-mundo/src/main/java/com/ib/Sumadora.java
@@ -0,0 +1,11 @@
+package com.ib;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sumadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
